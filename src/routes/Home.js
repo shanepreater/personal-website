1702129e11cf9f9bd9ef405153fcf4b4991ef8d5
Shane <shane.preater@gmail.com>
2019-10-11 12:02:52 +0100

    Fixed testing
    Added some tests and got it to work in the Jenkinsfile.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,10 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import BAESystemsLogo from "../static/companies/baesystems.svg"
+import RaytheonLogo from "../static/companies/raytheon.png"
+import LockheedLogo from "../static/companies/LM_logo_grey.svg"
+import FutureLogo from "../static/companies/future.png"
+import ThalesLogo from "../static/companies/thales.svg"
 
 const Home = (props) => (
     <div id="home">
@@ -10,6 +15,14 @@ const Home = (props) => (
         <p>Many of my roles have involved leading the design of complex systems.</p>
         <p>I have done many roles that required training / mentoring indivdual developers or
             complete development teams.</p>
+        <p>I have provided consultancy services in various capacities to many companies, including:</p>
+        <ul>
+            <li><BAESystemsLogo/></li>
+            <li><RaytheonLogo/></li>
+            <li><LockheedLogo/></li>
+            <li><FutureLogo/></li>
+            <li><ThalesLogo/></li>
+        </ul>
     </div>
 );
 
